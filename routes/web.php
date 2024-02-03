b7bfa00a16a5d49718607c51615a7a272c886711
wakhid13 <wakhidahsanulqoshi13@gmail.com>
2024-02-04 00:29:56 +0700

    tambah komentar

--- routes/web.php
+++ routes/web.php
@@ -27,37 +27,44 @@ Route::get('dashboard', function () {
 });
 
 Route::group(['middleware' => 'auth'], function () {
+    
+    // kategori
     Route::resource('categories','CategoryController');
     Route::get('/apiCategories','CategoryController@apiCategories')->name('api.categories');
     Route::get('/exportCategoriesAll','CategoryController@exportCategoriesAll')->name('exportPDF.categoriesAll');
     Route::get('/exportCategoriesAllExcel','CategoryController@exportExcel')->name('exportExcel.categoriesAll');
 
+    // produk
     Route::resource('products','ProductController');
     Route::get('/apiProducts','ProductController@apiProducts')->name('api.products');
 
+    // pelanggan
     Route::resource('customers','CustomerController');
     Route::get('/apiCustomers','CustomerController@apiCustomers')->name('api.customers');
     Route::post('/importCustomers','CustomerController@ImportExcel')->name('import.customers');
     Route::get('/exportCustomersAll','CustomerController@exportCustomersAll')->name('exportPDF.customersAll');
     Route::get('/exportCustomersAllExcel','CustomerController@exportExcel')->name('exportExcel.customersAll');
 
+    // supplier
     Route::resource('suppliers','SupplierController');
     Route::get('/apiSuppliers','SupplierController@apiSuppliers')->name('api.suppliers');
     Route::post('/importSuppliers','SupplierController@ImportExcel')->name('import.suppliers');
     Route::get('/exportSupplierssAll','SupplierController@exportSuppliersAll')->name('exportPDF.suppliersAll');
     Route::get('/exportSuppliersAllExcel','SupplierController@exportExcel')->name('exportExcel.suppliersAll');
 
-    Route::resource('productsOut','ProductKeluarController');
-    Route::get('/apiProductsOut','ProductKeluarController@apiProductsOut')->name('api.productsOut');
-    Route::get('/exportProductKeluarAll','ProductKeluarController@exportProductKeluarAll')->name('exportPDF.productKeluarAll');
-    Route::get('/exportProductKeluarAllExcel','ProductKeluarController@exportExcel')->name('exportExcel.productKeluarAll');
-    Route::get('/exportProductKeluar/{id}','ProductKeluarController@exportProductKeluar')->name('exportPDF.productKeluar');
-
+    // produk masuk
     Route::resource('productsIn','ProductMasukController');
     Route::get('/apiProductsIn','ProductMasukController@apiProductsIn')->name('api.productsIn');
     Route::get('/exportProductMasukAll','ProductMasukController@exportProductMasukAll')->name('exportPDF.productMasukAll');
     Route::get('/exportProductMasukAllExcel','ProductMasukController@exportExcel')->name('exportExcel.productMasukAll');
     Route::get('/exportProductMasuk/{id}','ProductMasukController@exportProductMasuk')->name('exportPDF.productMasuk');
+    
+    // produk keluar
+    Route::resource('productsOut','ProductKeluarController');
+    Route::get('/apiProductsOut','ProductKeluarController@apiProductsOut')->name('api.productsOut');
+    Route::get('/exportProductKeluarAll','ProductKeluarController@exportProductKeluarAll')->name('exportPDF.productKeluarAll');
+    Route::get('/exportProductKeluarAllExcel','ProductKeluarController@exportExcel')->name('exportExcel.productKeluarAll');
+    Route::get('/exportProductKeluar/{id}','ProductKeluarController@exportProductKeluar')->name('exportPDF.productKeluar');
 });
 
 
